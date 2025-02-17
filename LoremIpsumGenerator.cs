bd7998e33778261f5ee287f29302dbf9a5e813eb
KarbonKitty <308lapua.magnum@gmail.com>
2025-02-17 19:09:47 +0100

    Basic word length frequency

--- LoremIpsumGenerator.cs
+++ LoremIpsumGenerator.cs
@@ -6,33 +6,58 @@ public class LoremIpsumGenerator
 {
     private static readonly Dictionary<char, double> letterFrequency = new()
     {
-        { 'a', 0.082 },
-        { 'b', 0.015 },
-        { 'c', 0.028 },
-        { 'd', 0.043 },
-        { 'e', 0.127 },
-        { 'f', 0.022 },
-        { 'g', 0.020 },
-        { 'h', 0.061 },
-        { 'i', 0.070 },
-        { 'j', 0.0015 },
-        { 'k', 0.0077 },
-        { 'l', 0.040 },
-        { 'm', 0.024 },
-        { 'n', 0.067 },
-        { 'o', 0.075 },
-        { 'p', 0.019 },
-        { 'q', 0.001 },
-        { 'r', 0.060 },
-        { 's', 0.063 },
-        { 't', 0.091 },
-        { 'u', 0.028 },
-        { 'v', 0.001 },
-        { 'w', 0.024 },
-        { 'x', 0.0015 },
-        { 'y', 0.020 },
-        { 'z', 0.001 }
+        ['a'] = 0.082,
+        ['b'] = 0.015,
+        ['c'] = 0.028,
+        ['d'] = 0.043,
+        ['e'] = 0.127,
+        ['f'] = 0.022,
+        ['g'] = 0.020,
+        ['h'] = 0.061,
+        ['i'] = 0.070,
+        ['j'] = 0.0015,
+        ['k'] = 0.0077,
+        ['l'] = 0.040,
+        ['m'] = 0.024,
+        ['n'] = 0.067,
+        ['o'] = 0.075,
+        ['p'] = 0.019,
+        ['q'] = 0.001,
+        ['r'] = 0.060,
+        ['s'] = 0.063,
+        ['t'] = 0.091,
+        ['u'] = 0.028,
+        ['v'] = 0.001,
+        ['w'] = 0.024,
+        ['x'] = 0.0015,
+        ['y'] = 0.020,
+        ['z'] = 0.001,
     };
+
+    private static readonly double totalFrequency = letterFrequency.Values.Sum();
+
+    // from: https://core.ac.uk/download/pdf/82753461.pdf
+    private static readonly Dictionary<int, double> wordLengthFrequency = new()
+    {
+        [1] = 1000,
+        [2] = 7000,
+        [3] = 8000,
+        [4] = 7000,
+        [5] = 4000,
+        [6] = 3000,
+        [7] = 3000,
+        [8] = 2000,
+        [9] = 1500,
+        [10] = 1000,
+        [11] = 600,
+        [12] = 400,
+        [13] = 200,
+        [14] = 100,
+        [15] = 50,
+        [16] = 50
+    };
+
+    private static readonly double totalWordFrequency = wordLengthFrequency.Values.Sum();
 
     public static string Generate()
     {
@@ -46,17 +71,30 @@ public class LoremIpsumGenerator
 
         return paragraphBuilder.ToString();
     }
-    
-    private static readonly double totalFrequency = letterFrequency.Values.Sum();
-    
+
+
     private static Span<char> GenerateWord()
     {
-        var length = Random.Shared.Next(2, 15);
-        Span<char> word = new char[length + 1];
-        for (var i = 0; i <= length; i++)
+        var length = 0;
+        var r = Random.Shared.NextDouble() * totalWordFrequency;
+
+        var runningTotal = 0.0;
+        foreach (var (l, f) in wordLengthFrequency)
+        {
+            runningTotal += f;
+            if (runningTotal >= r)
+            {
+                length = l;
+                break;
+            }
+        }
+        
+        Span<char> word = new char[length];
+        for (var i = 0; i < length; i++)
         {
             word[i] = GenerateCharacter();
         }
+
         return word;
     }
 
@@ -67,6 +105,7 @@ public class LoremIpsumGenerator
         {
             r = Random.Shared.NextDouble();
         }
+
         var runningTotal = 0.0;
         foreach (var (c, w) in letterFrequency)
         {
@@ -76,6 +115,7 @@ public class LoremIpsumGenerator
                 return c;
             }
         }
+
         throw new InvalidOperationException("We should have returned something!");
     }
 }
